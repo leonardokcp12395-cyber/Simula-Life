500cb442319ce91eb6a9607992a137d172c12b53
leonardokcp12395-cyber <leonardokcp12395@gmail.com>
2025-09-18 11:00:47 -0300

    Create world_management.py

---
+++ core/world_management.py
@@ -0,0 +1,11 @@
+import noise
+import random
+from settings import GRID_WIDTH, GRID_HEIGHT, SCALE, OCTAVES, PERSISTENCE, LACUNARITY, TERRAINS, DAY_LENGTH, SEASON_LENGTH
+from entities.food import Food
+
+def generate_world(seed):
+    # ... (Cole a função generate_world aqui)
+
+def manage_environment(time_info, foods, world):
+    # ... (Cole a função manage_environment aqui, fazendo-a receber e retornar o estado do tempo)
+    # Ex: time_info['world_time'] += 1
